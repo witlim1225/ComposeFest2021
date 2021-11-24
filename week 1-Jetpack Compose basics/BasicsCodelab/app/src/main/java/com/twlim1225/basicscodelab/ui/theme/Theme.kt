9912d320fdc8d7e3fae10c29e59164b9b0f6827b
witlim1225 <witlim1225@gmail.com>
2021-11-24 09:20:34 +0900

    1주차 폴더 변경 시도 / 2주차 기타 파일 정리 시도.

---
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/twlim1225/basicscodelab/ui/theme/Theme.kt
@@ -0,0 +1,44 @@
+package com.twlim1225.basicscodelab.ui.theme
+
+import android.hardware.lights.Light
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = LightBlue,
+    surface = Blue,
+    onPrimary = Navy,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.White,
+
+)
+
+@Composable
+fun BasicsCodelabTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
